604b859bcd51d2eb259b82b6175e042d92eabc43
joe <jlx@ualberta.ca>
2021-04-24 03:36:17 +0000

    .

--- competition2/src/mazecralwer.py
+++ competition2/src/mazecralwer.py
@@ -24,6 +24,7 @@ longsleep = False
 
 laser_subscriber = None
 velocity_publisher = None
+scale = 2
 
 def setvelocity(newvelocity):
     velocity_publisher.publish(newvelocity)
@@ -35,15 +36,15 @@ def laser_handler(msg):
     #msg.ranges
     laserData = msg
     newvelocity = Twist()
-    newvelocity.linear.x = 0.3
+    newvelocity.linear.x = 0.3*scale
     newvelocity.angular.z = 0
     longsleep = False
     # wayy too close to right wall turn left
     if laserData.ranges[180] < 0.10 or laserData.ranges[0] < 0.10:
         newvelocity.angular.x = 0
-        newvelocity.angular.z = 0.25
+        newvelocity.angular.z = 0.25*scale
         if laserData.ranges[180] > 1.5:
-            newvelocity.angular.x = 0.2
+            newvelocity.angular.x = 0.2*scale
         velocity_publisher.publish(newvelocity)
         print("too close to right")
         return 
@@ -52,18 +53,18 @@ def laser_handler(msg):
         # break on right wall turn right
         
         if  laserData.ranges[90] > 0.5 or laserData.ranges[0] > 0.5:
-            newvelocity.linear.x = 0.15
-            newvelocity.angular.z = -0.5
+            newvelocity.linear.x = 0.15*scale
+            newvelocity.angular.z = -0.5*scale
             print("turning right")
             velocity_publisher.publish(newvelocity)
             return 
         # close enough to solid right wall
         if  (laserData.ranges[180] < 0.5 or laserData.ranges[0] < 0.5) :
-            newvelocity.linear.x = 0.3
+            newvelocity.linear.x = 0.3*scale
             newvelocity.angular.z = 0
             # too close to wall turn a bit left 
             if laserData.ranges[180] < 0.3 or laserData.ranges[90] <0.3 or laserData.ranges[0] < 0.30:
-                newvelocity.angular.z = 0.3
+                newvelocity.angular.z = 0.3*scale
                 print("slight left")
                 
             print("go streight")
@@ -72,8 +73,8 @@ def laser_handler(msg):
         
         # drifting left turn right 
         if  laserData.ranges[180] >= 0.5:
-            newvelocity.linear.x = 0.3
-            newvelocity.angular.z = -0.15
+            newvelocity.linear.x = 0.3*scale
+            newvelocity.angular.z = -0.15*scale
             print("glid right")
             velocity_publisher.publish(newvelocity)
             return 
@@ -83,7 +84,7 @@ def laser_handler(msg):
     # if something is in front and nothing on either side turn left
     if (laserData.ranges[360] <= 0.7 or laserData.ranges[270] <=0.3 ) and laserData.ranges[0] > 0.5 and laserData.ranges[719] > 0.5:
         newvelocity.linear.x = 0
-        newvelocity.angular.z = 0.7
+        newvelocity.angular.z = 0.7*scale
         print("wall ahead and nothing beside me turn left")
         velocity_publisher.publish(newvelocity)
         return
@@ -91,7 +92,7 @@ def laser_handler(msg):
     # if something is in front and nothing on my right turn right
     if laserData.ranges[360] <= 0.7 and laserData.ranges[0] > 0.5 and laserData.ranges[180] > 0.5 :
         newvelocity.linear.x = 0
-        newvelocity.angular.z = -0.7
+        newvelocity.angular.z = -0.7*scale
         print("wall ahead and nothing right turn right")
         velocity_publisher.publish(newvelocity)
         return 
@@ -99,7 +100,7 @@ def laser_handler(msg):
     # if something is in front and something on my right turn left
     if laserData.ranges[360] <= 0.7 and laserData.ranges[0] <= 0.5:
         newvelocity.linear.x = 0
-        newvelocity.angular.z = 0.7
+        newvelocity.angular.z = 0.7*scale
         print("wall ahead and right blocked")
         velocity_publisher.publish(newvelocity)
         return
